7b10af9b8ab0613f0ac1cbe0ef3b45a8500839bc
17001380 <17001380@galileo.edu>
2023-02-26 22:54:17 -0600

    Busqueda

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from .models import TecMembers
+from itertools import chain
+from django.db.models import Q
 # Create your views here.
 
 
@@ -18,8 +20,24 @@ def about(request):
 def search(request):
     if request.method == "POST":
         Busqueda = request.POST['Busqueda']
+        queryset = TecMembers.objects.filter(
+            Q(Clas1__contains=Busqueda) | 
+            Q(Sub_Clas1__contains=Busqueda) | 
+            Q(Sub_Clas2__contains=Busqueda)| 
+            Q(Sub_Clas3__contains=Busqueda)| 
+            Q(Sub_Clas4__contains=Busqueda)| 
+            Q(Specific__contains=Busqueda)
+        )
+        # filter1 = TecMembers.objects.filter(Clas1__contains=Busqueda)
+        # filter2 = TecMembers.objects.filter(Sub_Clas1__contains=Busqueda)
+        # filter3 = TecMembers.objects.filter(Sub_Clas2__contains=Busqueda)
+        # filter4 = TecMembers.objects.filter(Sub_Clas3__contains=Busqueda)
+        # filter5 = TecMembers.objects.filter(Sub_Clas4__contains=Busqueda)
+        # filter6 = TecMembers.objects.filter(Specific__contains=Busqueda)
+
         Context = {
-            'posts': TecMembers.objects.filter(Clas1__contains=Busqueda)
+            
+            'posts': queryset
         }
         
         return render(request, 'blog/Search.html',Context)
